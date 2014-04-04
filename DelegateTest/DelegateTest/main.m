66b5410aac9eb4aa353ea3f495a8514474da1579
Ysan521 <ysan_hfut@msn.cn>
2014-04-04 14:25:06 +0800

    delegate

---
+++ DelegateTest/DelegateTest/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  DelegateTest
+//
+//  Created by Ysan on 14-4-4.
+//  Copyright (c) 2014年 Ysan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Owner.h"
+#import "Delegate.h"
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        [[Delegate alloc]init];        
+    }
+    return 0;
+}
+
